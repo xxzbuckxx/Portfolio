10bfd088bf52f1a688a6a9ee62897d2e3bf3d3f3
Zack <ztraczyk.coding@gmail.com>
2018-05-24 15:26:33 -0500

    structure fixed

---
+++ Java/assets/examples/Fraction/Comparison.java
@@ -0,0 +1,12 @@
+
+/**
+ * Write a description of interface Comparison here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public interface Comparison
+{
+    boolean greaterThan(Object other);
+    boolean lessThan(Object other);
+}
